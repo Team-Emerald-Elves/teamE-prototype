4d00ae1452707a116f1ee85dd8d116ca7b5df2a2
clnteahan <colin@teahan.us>
2026-03-31 18:57:22 -0400

    Employee response sample (not implementation)

--- server/routes/employee.ts
+++ server/routes/employee.ts
@@ -1,7 +1,23 @@
 import express from "express";
 
+interface Employee {
+    id: number;
+    firstName: string;
+    lastName: string;
+    username: string;
+    email?: string;
+    roles?: string[];
+}
+
 function employeeRoute(req: express.Request, res: express.Response) {
-    res.send("employee");
+    let employee: Employee = {
+        id: 0,
+        firstName: "fname",
+        lastName: "lname",
+        username: "uname",
+        email: "flname@company.com",
+    }
+    res.json(employee);
 }
 
 export default employeeRoute;

336c0c42c9095a2900b53baea122ea1925c29445
brycenp28 <brycenq15@gmail.com>
2026-03-31 19:23:02 -0400

    Supabase & pnpm format fix

---
+++ apps/server/src/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+// Create a single supabase client for interacting with your file storage
+const supabase = createClient(
+	process.env.SUPABASE_URL,
+	process.env.SUPABASE_SECRET_KEY,
+);

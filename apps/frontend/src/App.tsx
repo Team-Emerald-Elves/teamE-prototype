392bd3cb7e773f75e2ab6640f729966ce1bc4584
anastasiak36 <akelnik.9@gmail.com>
2026-03-31 19:57:44 -0400

    moved client and server into apps folder and renamed them to front and back end

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/home.tsx';
+import Documents from './pages/documents.tsx';
+import EmployeeManagement from "./pages/employee.tsx"
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/"> Home </Link>
+        <Link to="/documents"> Documents </Link>
+        <Link to="/employee-management"> Employee Management </Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/documents" element={<Documents />} />
+        <Route path="/employee-management" element={<EmployeeManagement />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App

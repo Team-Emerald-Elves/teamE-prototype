70369dd5c3f20e34ef1301d6130dcc949b170ecf
clnteahan <colin@teahan.us>
2026-04-01 17:05:15 -0400

    employees and create employees

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -4,6 +4,8 @@ import path from 'path';
 import employeeRoute from "./routes/employee.ts";
 import serviceReqRoute from "./routes/servicereqs.ts";
 import assignedRoute from "./routes/assigned.ts";
+import createEmployeeRoute from "./routes/create-employee.ts";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = parseInt(process.env.PORT!) || 3000;
@@ -20,6 +22,8 @@ console.log(__dirname);
 
 //app.use("/public", express.static('public'));
 
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.sendStatus(200);
 })
@@ -29,6 +33,8 @@ app.get('/employee', employeeRoute);
 app.get('/servicereqs', serviceReqRoute)
 
 app.get('/assigned', assignedRoute);
+
+app.post('/create-employee', createEmployeeRoute);
 
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}!`);
--- apps/backend/src/routes/employee.ts
+++ apps/backend/src/routes/employee.ts
@@ -1,10 +1,9 @@
 import express from "express";
 import {prisma} from "../lib/prisma.ts";
-import bodyParser from "body-parser";
 
 
 interface IEmployee {
-    id: number;
+    id: string;
     firstName: string;
     lastName: string;
     username: string;
@@ -16,23 +15,14 @@ function employeeRoute(req: express.Request, res: express.Response) {
 
     prisma.employee.findMany({
         orderBy: {
-            name: "asc"
+            first_name: "asc"
         }
     }).then((value) => {
-        console.log("Employees returned")
-        console.log("Employees: ", value);
+        res.json(value);
     }, (err) => {
         console.error("[ERROR]", err);
     });
 
-    let employee: IEmployee = {
-        id: 0,
-        firstName: "fname",
-        lastName: "lname",
-        username: "uname",
-        email: "flname@company.com",
-    }
-    res.json(employee);
 }
 
 export default employeeRoute;

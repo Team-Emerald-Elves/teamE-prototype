033d1c8bad61175db4f9fd0bd9d42431419fb9d3
alexmlaw11-cloud <alexmlaw11@gmail.com>
2026-04-01 18:50:29 -0400

    hero image

--- apps/frontend/src/pages/home.tsx
+++ apps/frontend/src/pages/home.tsx
@@ -1,6 +1,7 @@
 import Card from "../components/card.tsx";
 import "./home.css";
 import {SearchBar} from "@/components/searchbar.tsx";
+import img from "./hanoverinsurence.webp";
 
 
 
@@ -13,6 +14,9 @@ function Home() {
             <div className="search-container">
                 <SearchBar />
             </div>
+            <div className={ "hero-container"}>
+                <img src = {img} alt = "hanoverPic"/>
+                </div>
             <div style={{ marginBottom: '1.5rem' }}></div>
             <div className="home-cards-container">
             <Card title={"Placeholder Card Title"}></Card>

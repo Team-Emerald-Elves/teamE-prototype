19dc5543013225e298279401a33bcc5a65f67268
jenelialeo <jenelialeo@gmail.com>
2026-04-02 18:12:44 -0400

    resized doc cards grid

--- apps/frontend/src/pages/documents.tsx
+++ apps/frontend/src/pages/documents.tsx
@@ -71,7 +71,7 @@ function Documents(props: docProps) {
 
                 </div>
 
-                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                     <DocumentCard name="Business Requirements" type="Workflow" />
                     <DocumentCard name="System Requirements" type="Workflow"/>
                     <DocumentCard name="Process Flow Diagrams" type="Workflow"/>

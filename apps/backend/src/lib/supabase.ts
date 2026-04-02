723cabc43c823667dc42c7b97fff3798f5936629
brycenp28 <brycenq15@gmail.com>
2026-04-02 13:31:03 -0400

    (Supabase): Added integrations for file handling with backend secret key.

---
+++ apps/backend/src/lib/supabase.ts
@@ -0,0 +1,18 @@
+import { createClient } from '@supabase/supabase-js'
+import dotenv from "dotenv";
+dotenv.config();
+
+const supabaseURL = process.env.PUBLIC_SUPABASE_URL as string
+const supabaseKey = process.env.SUPABASE_SECRET_KEY as string
+
+// Create a single supabase client for interacting with your database
+// server/api route: per request
+export function createSupabaseForRequest() {
+    return createClient(supabaseURL, supabaseKey)
+}
+
+export function createSupabaseForRequestJWT(jwt: string) {
+  return createClient(supabaseURL, supabaseKey, {
+    accessToken: async () => jwt,
+  })
+}

793adba6298933401d167101479e89782b4b61df
anastasiak36 <akelnik.9@gmail.com>
2026-04-02 18:32:24 -0400

    added clear form button

--- apps/frontend/src/components/contentForm.tsx
+++ apps/frontend/src/components/contentForm.tsx
@@ -42,10 +42,14 @@ function ContentForm(props: contentFormProps) {
     return (
         <Dialog>
             <form>
-                <DialogTrigger render={<Button variant="outline" size={ props.size ? "lg" : "sm"} className={props.size ? "px-6 py-3.5 text-lg bg-secondary text-secondary-foreground": "px-4 py-3 text-base bg-secondary text-secondary-foreground"} >{props.type}</Button>} />
+                <DialogTrigger render={<Button variant="outline" className={props.size ? "px-6 py-3.5 text-lg bg-secondary text-secondary-foreground": "px-4 py-3 text-base bg-secondary text-secondary-foreground"} >{props.type}</Button>} />
                 <DialogContent className="lg:max-w-lg">
                     <DialogHeader>
-                        <DialogTitle className="text-2xl text-primary font-mono font-bold">{props.type} Content</DialogTitle>
+                        <div className="flex items-center justify-between p-2">
+                            <DialogTitle className="text-2xl text-primary font-mono font-bold">{props.type} Content</DialogTitle>
+                            <Button variant="outline" size="lg" className="bg-primary text-primary-foreground">Clear</Button>
+                        </div>
+
                     </DialogHeader>
                     <FieldGroup>
                         <Field>

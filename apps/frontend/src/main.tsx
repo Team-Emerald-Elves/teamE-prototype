031d15029dd2976a3185abaa31d8a084eb2dd422
brycenp28 <brycenq15@gmail.com>
2026-04-06 18:30:44 -0400

    (Auth): Added clerk dependencies and auth hook.

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,17 +3,21 @@ import { createRoot } from 'react-dom/client'
 import {ClerkProvider} from "@clerk/clerk-react";
 import './index.css'
 import App from './App.tsx'
+import dotenv from 'dotenv'
 
-function RootLayout() {
-    return (
-        <ClerkProvider>
-            <App />
-        </ClerkProvider>
-    )
+dotenv.config()
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = process.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
 }
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <RootLayout />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
 )

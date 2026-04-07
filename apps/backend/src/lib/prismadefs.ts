fdbb6ca51f0fb0c6ae95b8152c44cad8526d63da
Colin Teahan <94491724+clnteahan@users.noreply.github.com>
2026-04-07 12:18:11 -0400

    all good

--- apps/backend/src/lib/prismadefs.ts
+++ apps/backend/src/lib/prismadefs.ts
@@ -1,4 +1,4 @@
-import { type Employee, ServiceRequests } from "../../prisma/generated/client";
+import { type Employee, type ServiceRequests } from "../../prisma/generated/client.ts";
 
 export type Employee = Employee;
 export type ServiceRequests = ServiceRequests;

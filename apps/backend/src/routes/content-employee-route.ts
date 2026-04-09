c473049ad6ac65e89815e84e4f258ceb79e63e92
Elliot-WPI <127912326+Elliot-WPI@users.noreply.github.com>
2026-04-08 20:50:08 -0400

    Revert "Content fixed"

--- apps/backend/src/routes/content-employee-route.ts
+++ apps/backend/src/routes/content-employee-route.ts
@@ -12,14 +12,7 @@ interface IEmployeeID {
 
 function contentEmployeeRoute(req: express.Request, res: express.Response) {
     const employee: IEmployeeID = req.body as IEmployeeID;
-    prisma.fileContent.findMany({
-        where: {
-            bucket: {
-                employeeId: employee.id
-            }
-        }
-    })
-        .then((data) => {
+    prisma.content.findMany({where: {employeeId: employee.id}}).then((data) => {
         res.json(data)
     }).catch((err) => {
         console.log("Error: ", err)
--- apps/backend/src/routes/create-content.ts
+++ apps/backend/src/routes/create-content.ts
@@ -3,26 +3,25 @@ import { prisma } from "../lib/prisma.ts";
 
 interface IContentCreator {
     name: string;
-    url: string;
-    bucket: string;
+    URL: string;
+    job_position: string;
     expiration_date: string;
     content_type?: string;
     document_status?: string;
     employeeId: string;
-    mime_type?: string;
 }
 
 function createContentRoute(req: express.Request, res: express.Response) {
     const content: IContentCreator = req.body as IContentCreator;
-    prisma.fileContent
+    prisma.content
         .create({
             data: {
                 name: content.name,
-                url: content.url,
-                bucket: content.bucket,
+                URL: content.URL,
+                job_position: content.job_position,
                 expiration_date: new Date(content.expiration_date),
                 content_type: content.content_type,
-                mime_type: content.mime_type,
+                document_status: content.document_status,
                 content_owner: {
                     connect: { id: content.employeeId },
                 },

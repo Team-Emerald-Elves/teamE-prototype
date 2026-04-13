599ce50e76d3e87d57bc0d3f1b5d162c7a3d2dd6
alexmlaw11-cloud <alexmlaw11@gmail.com>
2026-04-13 00:14:43 -0400

    created documents table

--- apps/frontend/src/pages/documents.tsx
+++ apps/frontend/src/pages/documents.tsx
@@ -1,22 +1,69 @@
-import DocumentCard from '../components/docCard.tsx'
 import ContentForm from '../components/contentForm.tsx'
 import {SearchBar} from '../components/searchbar.tsx'
 import {useState, useEffect} from "react";
 import {getToken, useAuth} from "@clerk/react"
+import {TestDoc} from "../components/testdoctable.tsx"
+import { columns, type UserDocuments } from "../components/docCols.tsx"
 
 
 type Document = {
-    name: string,
-    url: string,
-    id: number,
-    bucketID: string,
-    lastModified: string,
-    expirationDate: string,
-    mimeType: string,
-    documentStatus: number,
+    id: number;
+    url: string;
+    name: string;
+    lastModified: string;
+    expirationDate: string;
+    mime_type: string;
+    role: string;
+    contentOwner: string;
+    status: string;
+};
 
+const Data: Document[] = [];
+
+const Doc1: Document = {
+    id: 1,
+    url: "www.testurl.com",
+    name: "underwritingrole",
+    lastModified: "01/02/2025",
+    expirationDate: "01/02/2027",
+    mime_type: "reference",
+    role: "underwriter",
+    contentOwner: "leah",
+    status: "not_started",
+};
+
+const Doc2: Document = {
+    id: 2,
+    url: "www.testurl2.com",
+    name: "businessanalystrole1",
+    lastModified: "03/02/2025",
+    expirationDate: "03/02/2027",
+    mime_type: "reference",
+    role: "businessanalyst",
+    contentOwner: "john",
+    status: "not_started",
+};
+
+Data.push(Doc1);
+Data.push(Doc2);
+
+async function getData(): Promise<UserDocuments[]> {
+    // Fetch data from your API here.
+    return [
+        {
+            id: 2,
+            url: "www.testurl2.com",
+            name: "businessanalystrole1",
+            lastModified: "03/02/2025",
+            expirationDate: "03/02/2027",
+            mime_type: "reference",
+            role: "businessanalyst",
+            contentOwner: "john",
+            status: "not_started",
+        },
+        // ...
+    ]
 }
-
 
 async function getDocuments(token: string) {
     const res = await fetch(
@@ -52,8 +99,9 @@ async function getDocumentsAdmin(token: string) {
 }
 
 
-function Documents() {
+export default function Documents() {
     const [roles, setRoles] = useState<string[]>([]);
+    const[docs, setDocs] = useState<Document[]>([]);
     const { getToken, isSignedIn } = useAuth();
     const [me, setMe] = useState(null);
 
@@ -80,7 +128,6 @@ function Documents() {
         load();
     }, [isSignedIn, roles]);
 
-    const [documents, setDocuments] = useState([]);
     const [sessionToken, setSessionToken] = useState("")
 
     useEffect(() => {
@@ -100,7 +147,7 @@ function Documents() {
                 ? await getDocumentsAdmin(sessionToken)
                 : await getDocuments(sessionToken);
 
-            setDocuments(docsData);
+            setDocs(docsData);
         };
 
         fetchData();
@@ -137,21 +184,6 @@ function Documents() {
 
                 </div>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-
-                    {documents.map((doc:Document) => (
-                        <div key={doc.id}>
-                            <DocumentCard document={doc} name={doc.name} type="Reference" />
-                        </div>
-
-                    ))}
-                    {/*<DocumentCard name="Underwriting Rules" type="Reference" />*/}
-                    {/*<DocumentCard name="Approved Filings" type="Reference" />*/}
-                    {/*<DocumentCard name="State Guidelines" type="Reference"/>*/}
-                    {/*<DocumentCard name="Use Cases" type="Reference"/>*/}
-
-                </div>
-
             </>
         )
     }
@@ -161,43 +193,11 @@ function Documents() {
                 <div className="text-center font-bold text-primary">
                     <h1 className="font-mono">Documents</h1>
                 </div>
-                <div className="flex items-center w-full p-4">
-                    <div className="flex-1 flex ">
-                        <SearchBar />
-                    </div>
-                    <div className="ml-auto  p-4">
-                        <ContentForm
-                            type="Create"
-                            currentID={Math.trunc((Math.random() * 10000) % 10000)}
-                            currentName="Name..."
-                            currentURL="www.example.com"
-                            currentContentOwner="Select Content Owner"
-                            currentRole="Select Role"
-                            currentExpirationDate="Tomorrow"
-                            currentExpirationTime="10:30:00"
-                            currentStatus="Select Status"
-                            size={true}
-                        />
-                    </div>
-
+                <div>
+                    <TestDoc columns={columns} data={Data} />
                 </div>
-
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-                    {documents.map((doc:Document) => (
-                        <DocumentCard document={doc} name={doc.name} type="Reference" />
-                    ))}
-                    {/*<DocumentCard name="Business Requirements" type="Workflow" />*/}
-                    {/*<DocumentCard name="System Requirements" type="Workflow"/>*/}
-                    {/*<DocumentCard name="Process Flow Diagrams" type="Workflow"/>*/}
-                    {/*<DocumentCard name="ACT Guide" type="Workflow"/>*/}
-                    {/*<DocumentCard name="Traceability Matrix" type="Workflow"/>*/}
-
-
-                </div>
-
 
             </>
         )
     }
 }
-export default Documents;

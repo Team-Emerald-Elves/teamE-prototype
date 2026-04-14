a05a5734b90b436b9f64ed8d05b360ec7c0e8f82
Zara Jaferi <zara.jaferi06@gmail.com>
2026-04-14 19:18:16 -0400

    simple ui (ugly)

--- apps/backend/src/routes/statistics.ts
+++ apps/backend/src/routes/statistics.ts
@@ -4,11 +4,15 @@ import {prisma} from "../lib/prisma.ts";
 
 async function statsRoutes(req: express.Request, res: express.Response) {
     try {
-        const [docCount, empCount] = await Promise.all([
+        const [docCount, empCount, docByStatus] = await Promise.all([
             prisma.documentContent.count(),
             prisma.employee.count(),
+            prisma.documentContent.groupBy({ //group doc content by status
+                by: ['document_status'],
+                _count: { id: true } // num rows per group
+            }),
         ])
-        return res.status(200).json({docCount, empCount})
+        return res.status(200).json({docCount, empCount, docByStatus})
     } catch(error){
         console.error("Failed to get data", error)
         return res.status(500).json(`{"message":"Failed to get data"}`)
--- apps/frontend/src/pages/statisticsPage.tsx
+++ apps/frontend/src/pages/statisticsPage.tsx
@@ -1,4 +1,7 @@
 import { useState, useEffect } from "react";
+import { FileText } from 'lucide-react';
+import { UsersRound } from 'lucide-react';
+import PageHeader from "../components/page-header.tsx"
 
 
 
@@ -8,10 +11,6 @@ type EmployeeStats = {
     loginAttempts: number;
 };
 
-type SimpleStats = {
-    docTotal: number;
-    empTotal: number;
-}
 
 const MOCK_STATS: EmployeeStats[] = [
     { employee: "Alice Chen", role: "Admin", loginAttempts: 42 },
@@ -26,6 +25,8 @@ const MOCK_STATS: EmployeeStats[] = [
 export default function StatisticsPage() {
     const [docTotal, setDocTotal] = useState(0);
     const [empTotal, setEmpTotal] = useState(0);
+    type StatusCount = { document_status: string, _count: { id: number } }
+    const [docByStatus, setDocByStatus] = useState<StatusCount[]>([]);
     useEffect(() => {
         async function getStats() {
             const res = await fetch(
@@ -35,6 +36,7 @@ export default function StatisticsPage() {
             const data = await res.json();
             setDocTotal(data.docCount);
             setEmpTotal(data.empCount);
+            setDocByStatus(data.docByStatus);
 
         }
         getStats()
@@ -42,10 +44,30 @@ export default function StatisticsPage() {
 
     return(
         <>
-            <h1> {docTotal}</h1>
-            <h1> {empTotal}</h1>
+            <PageHeader title="Reports & Statistics" description="View data on documents and employees."/>
+            <div className="flex mt-10 justify-center h-screen">
+                <div className="flex flex-col items-center justify-center rounded-xl shadow-sm w-[80%] h-50 bg-white p-6">
+                    <div className="flex align-middle items-center justify-between flex-row gap-10">
+                        <div className= "flex flex-row items-center">
+                            <FileText size={60} color="#013C5A"/>
+                            <div className="flex text-left flex-col pl-5">
+                                <h1> {docTotal}</h1>
+                                <p>Total Documents</p>
+                            </div>
+                        </div>
+                        <div className= "flex flex-row gap-0 items-center">
+                            <UsersRound size={60} color="#013C5A"/>
+                            <div className="flex text-left flex-col pl-5 gap-1">
+                                <h1> {empTotal}</h1>
+                                <p>Total Employees</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
         </>
     )
 }
-
 

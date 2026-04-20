b651db850ae30e2fcf6bd409371fbf401ee71b9f
brycenp28 <brycenq15@gmail.com>
2026-04-20 18:19:29 -0400

    (Patch): matched dotenv configs.

--- packages/database/prisma.config.ts
+++ packages/database/prisma.config.ts
@@ -1,7 +1,21 @@
-import 'dotenv/config';
+import dotenv from 'dotenv'
 import path from "node:path";
 import { defineConfig, env } from 'prisma/config';
 import { enviroments } from './lib/env.ts';
+import { fileURLToPath } from 'url';
+
+// 1. Create a reliable __dirname for ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load env files
+
+dotenv.config({
+    path: [
+        path.resolve(__dirname, '../../apps/backend/.env'),
+        path.resolve(__dirname, '.env')
+    ]
+})
 
 const dataBaseURL: string | null = env('NODE_ENV') in enviroments ? env(env('NODE_ENV').toUpperCase() + '_DIRECT_URL') : null
 

b35bf161531b2d589203556c6cc2c7ae49668947
SylviaTheFox <jstrayer@wpi.edu>
2026-04-21 21:36:51 -0400

    HOPE!

--- apps/backend/src/lib/filters.ts
+++ apps/backend/src/lib/filters.ts
@@ -5,16 +5,17 @@ export const buildWhereClause = (filters: any, additional: any) => {
     if (Object.entries(additional).length > 0) {
         whereClause.AND.push(additional);
     }
-
-    for (const [key, value] of Object.entries(filters)) {
-        if (value) {
-            const tempJSON: any = {OR: []};
-            for (const v of Object.entries(value)) {
-                const pushObject: any = {}
-                pushObject[key] = v[1];
-                tempJSON.OR.push(pushObject)
+    if (Object.entries(filters).length > 0) {
+        for (const [key, value] of Object.entries(filters)) {
+            if (value) {
+                const tempJSON: any = {OR: []};
+                for (const v of Object.entries(value)) {
+                    const pushObject: any = {}
+                    pushObject[key] = v[1];
+                    tempJSON.OR.push(pushObject)
+                }
+                whereClause.AND.push(tempJSON)
             }
-            whereClause.AND.push(tempJSON)
         }
     }
 

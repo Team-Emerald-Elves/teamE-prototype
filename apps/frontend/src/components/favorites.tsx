de083893634834053ee56585551658a58f2b450b
anastasiak36 <akelnik.9@gmail.com>
2026-04-26 19:08:29 -0400

    added document hit count table to schema and created at date to favorites

--- apps/frontend/src/components/favorites.tsx
+++ apps/frontend/src/components/favorites.tsx
@@ -116,6 +116,7 @@ export default function Favorites() {
                                         <TableHead
                                             className="text-[#0b4461] text-center font-medium text-sm">Favorite</TableHead>
                                         <TableHead className="text-[#0b4461] font-medium text-sm">Title</TableHead>
+                                        <TableHead className="text-[#0b4461] font-medium text-sm">Created</TableHead>
                                         <TableHead className="text-[#0b4461] font-medium text-sm">Content Type</TableHead>
                                         <TableHead className="text-[#0b4461] font-medium text-sm">Expiration Date</TableHead>
                                         <TableHead className="text-[#0b4461] font-medium text-sm">Status</TableHead>
